31131c243b843ef25f3945b543403cce2b4e122a
jillalynch <jill.lynch5@gmail.com>
2014-07-21 16:23:32 -0500

    Update oo_js.js

--- books_oo_js/oo_js.js
+++ books_oo_js/oo_js.js
@@ -1,47 +1,48 @@
 var libros = {
 
-  init: function() {
     // Instantiating a new object
-    var book = new Book({
+    book: new Book({
       title: "Robot Dreams",
       author: "Isaac Asimov",
       numPages: '320'
-    });
+    }),
 
     // Instantiating a new object
-    var paperbackbook = new Paperback({
+    paperbackbook: new Paperback({
       title: "Jaws",
       author: "Peter Benchley",
       numPages: 99
-    });
-
+    }),
+  
+  
+  init: function() {
     book.read();
     paperbackbook.read();
-  }
-
-  function Book(config) {
+  },
+  
+  Book: function(config) {
     config = config || {};
     this.title = config.title || "Untitled";
     this.author = config.author || "Unknown";
     this.numPages = config.numPages || 100;
     this.currentPage = 0;
-  }
+  },
 
   // The constructor function
-  function Paperback(title, author, numPages, cover) {
+  Paperback: function(title, author, numPages, cover) {
     Book.call(this, title, author, numPages);
     this.cover = cover;
-  }
+  },
 
   // A method on the object
-  Book.prototype.read = function() {
-    console.log();
-    $("body").append("<p>You read " + this.numPages + " pages of the title " + this.title + "!</p>");
-  };
+  //Book.prototype.read = function() {
+    //console.log();
+    //$("body").append("<p>You read " + this.numPages + " pages of the title " + this.title + "!</p>");
+  //},
 
   // Extending the Book object
-  Paperback.prototype = Object.create(Book.prototype);
+  //Paperback.prototype = Object.create(Book.prototype);
 
-  libros.init();
+};
 
-}
+libros.init();
